7f64c313f1b867396da7b19fef2ae3250a68de8e
unknown <zirzirlove96@github.com>
2020-09-15 13:15:48 +0900

    update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from indeed import get_jobs as get_indeed_jobs
 #indeed.py의 함수를 사용하기 위해
+from so import get_jobs as get_so_jobs
 
-indeed_jobs = get_indeed_jobs()
+#indeed_jobs = get_indeed_jobs()
+so_jobs = get_so_jobs()#stackoverflow 웹 페이지 
 
-print(indeed_jobs)
 
--- so.py
+++ so.py
@@ -4,7 +4,21 @@ from bs4 import BeautifulSoup
 #LIMIT=50 stackoverflow페이지는 limit을 사용하지 않는다.
 URL=f"https://stackoverflow.com/jobs?q=python&pg=2"
 
+#마지막 페이지를 반환하는 함수 
+def get_last_page():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pages = soup.find("div",{"class":"s-pagination"}).find_all("a")
+    #div요소의 class이름이 s-pagination인 곳에서 a태그를 가지고 있는 모두를 가져온다.
+    last_page = pages[-2].get_text(strip=True)#Next표시를 없애고 마지막 페이지까지만 가져오기 위해
+    #string대신 get_text()를 사용하여 숫자만 가져오고 빈 여백을 없애기 위해 strip=True를 해준다.
+    return last_page
+    
+
+
 #페이지를 추출하기
 def get_jobs() :
     last_page = get_last_page()
+    print(last_page)
+    return []
     
