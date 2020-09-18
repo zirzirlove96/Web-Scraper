ad37d84ec95ee93155929441215d2b99cdeecc0a
unknown <zirzirlove96@github.com>
2020-09-18 17:43:01 +0900

    fake db create and data save into the fake db

--- flaskEx.py
+++ flaskEx.py
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
 from flask import Flask,render_template,request,redirect
 #request는 데이터를 웹사이트에 보내는 역할도 하게 된다.
 from so import get_jobs
-=======
-from flask import Flask,render_template,request
-#request는 데이터를 웹사이트에 보내는 역할도 하게 된다.
->>>>>>> 8594bc4b86c1e046ee924f8f9df44e2e6c2e7c76
-
 
 app = Flask("SuperScrapper")
+
+
+#Fake DB는 라우트 외부에 있어야 한다.
+db={}
+
 
 @app.route("/")#/로 접속
 def home():
@@ -16,33 +15,27 @@ def home():
     return render_template("index.html")#render_template을 사용하여 index.html을 사용하게 한다.
 
 @app.route("/react")
-<<<<<<< HEAD
 def report():
     word = request.args.get('word')#index.html에서 입력폼에 입력한 값을 받아올 수 있는 함수.
     #return f"your are looking for a job in {word}"
-    jobs = get_jobs(word)
-    """ so.py의 get_jobs 메서드를 사용 """
-    print(jobs)
+
+    fromdb = db.get(word)
+    if fromdb:#db에 이미 있으면
+        jobs = fromdb
+    else:
+        #db에 없으면 찾아서 db에 저장
+        jobs = get_jobs(word)# so.py의 get_jobs 메서드를 사용
+        db[word]=jobs
     if word=="":#입력 폼에 아무것도 입력하지 않았을 때 
         return redirect("/")
-    return render_template("report.html",searchingBy=word)#report.html에 값을 보내준다.
+    return render_template("report.html",number=len(jobs),searchingBy=word)#report.html에 값을 보내준다.
 
 """
-=======
-def react():
-    word = request.args.get("word")#index.html에서 입력폼에 입력한 값을 받아올 수 있는 함수.
-    #return f"your are looking for a job in {word}"
-    return render_template("react.html",searchingBy=word)#react.html에 단어를 보내준다.
-
->>>>>>> 8594bc4b86c1e046ee924f8f9df44e2e6c2e7c76
 @app.route("/contact")
 def contact():
     return "Contact!"#/contact 접속했을 경우
 #@에 의해 바로 밑에 있는 함수만을 봐서 수행한다.
-<<<<<<< HEAD
 """
-=======
->>>>>>> 8594bc4b86c1e046ee924f8f9df44e2e6c2e7c76
 
 """
 @app.route("/<username>")
--- so.py
+++ so.py
@@ -1,17 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-<<<<<<< HEAD
 
 #마지막 페이지를 반환하는 함수 
 def get_last_page(URL):
-=======
-#LIMIT=50 stackoverflow페이지는 limit을 사용하지 않는다.
-URL=f"https://stackoverflow.com/jobs?q=python&sort=i"
-
-#마지막 페이지를 반환하는 함수 
-def get_last_page():
->>>>>>> 8594bc4b86c1e046ee924f8f9df44e2e6c2e7c76
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div",{"class":"s-pagination"}).find_all("a")
@@ -38,11 +30,7 @@ def extract_job(html):
     
 
 #페이지에 대한 직업들 추출하는 함수(-job은 extract_job의 함수에서 반환하는 데이터가 있는 곳이다. )
-<<<<<<< HEAD
 def extract_jobs(last_page,URL):
-=======
-def extract_jobs(last_page):
->>>>>>> 8594bc4b86c1e046ee924f8f9df44e2e6c2e7c76
     jobs=[]
     for page in range(last_page):
         print(f"StackOverflow Web Scraping {page}")
@@ -57,7 +45,6 @@ def extract_jobs(last_page):
 
 
 #페이지를 추출하기
-<<<<<<< HEAD
 def get_jobs(word) : #flaskEx.py에서 입력된 데이터 값을 넣어 stackoverflow직업을 찾아
     #html에 나타내 준다.
     #LIMIT=50 stackoverflow페이지는 limit을 사용하지 않는다.
@@ -65,10 +52,5 @@ def get_jobs(word) : #flaskEx.py에서 입력된 데이터 값을 넣어 stackov
     """URL에 사용하고 싶은 단어를 사용하여 직업을 찾고 싶기 때문에"""
     last_page = get_last_page(URL)
     jobs = extract_jobs(last_page,URL)
-=======
-def get_jobs() :
-    last_page = get_last_page()
-    jobs = extract_jobs(20)
->>>>>>> 8594bc4b86c1e046ee924f8f9df44e2e6c2e7c76
     return jobs
     
